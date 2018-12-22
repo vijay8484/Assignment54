6983b189744584440f2041bd0d53b74f70f28f01
Your NameVijay <vijay8484patil@gmail.com>
2018-12-22 15:29:19 +0530

    added Assignmnment54

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,8 +2,17 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `
+    <style>
+    h1 {
+      color: blue;
+    }
+    </style>
+    <h1>
+   <app-new-comp></app-new-comp>
+
+    </h1>
+    `
 })
 export class AppComponent {
   title = 'Assignment54';
